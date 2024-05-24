6e8679fb632ec66ec75efd658c9abd86441c4a22
Vitor Sbeghi <126426164+SbgVit0r@users.noreply.github.com>
2024-05-23 21:55:03 -0300

    Aula Web API

--- scripts/buscar-encomendas.js
+++ scripts/buscar-encomendas.js
@@ -1,17 +1,26 @@
 var botaoBuscar = document.querySelector("#buscar-encomendas");
+var nomeCliente = document.querySelector(".info-nome");
 
 botaoBuscar.addEventListener("click", function(){
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "http://localhost:3000/encomendas_web");
+    xhr.open("GET", "https://fakestoreapi.com/products");
     xhr.addEventListener("load", function(){
         var resposta = xhr.responseText;
         
         var encomendas = JSON.parse(resposta);
         
         encomendas.forEach(function(encomenda){
-            addEncomenda(encomenda)
+            if(!clienteExiste()){
+                addEncomenda(encomenda);
+            } 
         })
+
     });
     xhr.send();
 })
+
+function clienteExiste(){
+
+    
+}
